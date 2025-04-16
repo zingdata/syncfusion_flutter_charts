cef9b64c24ee1fc06c54e1365c26b2b1af519c14
Raza Abbas <razankv13@gmail.com>
2025-04-16 15:11:23 +0500

    Reapply "Update axis.dart"
    This reverts commit b267cd6b0c22987efb425906c29e2bb22db2f2eb.

--- lib/src/charts/axis/axis.dart
+++ lib/src/charts/axis/axis.dart
@@ -4487,7 +4487,7 @@ class _HorizontalAxisRenderer extends _AxisRenderer {
         }
       }
 
-      Offset position = Offset(labelOffset.dx + label.position!, dy);
+      Offset position = Offset(labelOffset.dx + label.position!, dy + 12);
       label.region =
           Rect.fromLTWH(position.dx, position.dy, label.labelSize.width, label.labelSize.height);
       position += offset;
@@ -4818,13 +4818,19 @@ class _VerticalAxisRenderer extends _AxisRenderer {
       ..textAlign = TextAlign.center
       ..textDirection = TextDirection.ltr
       ..layout();
+
     if (rotationAngle == 0) {
       _textPainter.paint(context.canvas, position);
     } else {
+      // When labels are rotated, adjust their position to prevent overlap with chart
+      final double adjustY = rotationAngle == angle45Degree
+          ? label.labelSize.height * 0.3 // For 45-degree rotation, move down by 30% of height
+          : label.labelSize.height * 0.5; // For 90-degree rotation, move down by 50% of height
+
       context.canvas
         ..save()
-        ..translate(
-            position.dx + label.labelSize.width / 2, position.dy + label.labelSize.height / 2)
+        ..translate(position.dx + label.labelSize.width / 2,
+            position.dy + label.labelSize.height / 2 + adjustY)
         ..rotate(degreeToRadian(rotationAngle));
       _textPainter.paint(
           context.canvas, Offset(-_textPainter.size.width / 2, -_textPainter.size.height / 2));
